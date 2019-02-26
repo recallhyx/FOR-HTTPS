b6be0560ae05936890c923678f3db792d617e45b
雨霰 <yuxian.hyx@alibaba-inc.com>
2019-02-26 18:16:17 +0800

    feat: change reg

--- demo-ServiceWorker/sw.js
+++ demo-ServiceWorker/sw.js
@@ -78,18 +78,19 @@
 //     })
 //   );
 // });
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.3/workbox-sw.js');
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/3.0.0-alpha.3/workbox-sw.js"
+);
 
 if (workbox) {
-    console.log(`Yay! workbox is loaded 🎉`);
-    workbox.precaching.precacheAndRoute([
-      { url: '/index.html', revision: '383676' },
-  ]);
-  //   workbox.routing.registerRoute(
-  //     new RegExp('.*\.html'), // 匹配的路由
-  //     workbox.strategies.networkFirst()
-  // );
-}
-else {
-    console.log(`Boo! workbox didn't load 😬`);
+  console.log(`Yay! workbox is loaded 🎉`);
+  // workbox.precaching.precacheAndRoute([
+  //   { url: "/index.html", revision: "383676" }
+  // ]);
+  workbox.routing.registerRoute(
+    "/index.html", // 匹配的路由
+    workbox.strategies.networkFirst()
+  );
+} else {
+  console.log(`Boo! workbox didn't load 😬`);
 }
